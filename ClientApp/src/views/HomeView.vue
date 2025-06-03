4b1af45e4ceb00da79beb868e990c35ad8c06696
jonas <jonasroder@gmail.com>
2025-06-03 20:01:03 -0300

    teste final depoly 2

--- ClientApp/src/views/HomeView.vue
+++ ClientApp/src/views/HomeView.vue
@@ -4,7 +4,7 @@
             <v-col cols="12" md="8">
                 <v-card elevation="2">
                     <v-card-title class="headline">
-                        Previsão tempo com deploy automatico
+                        Previsão tempo com deploy automatico com cache
                     </v-card-title>
                     <v-card-text>
                         <v-btn color="primary" @click="loadForecasts">
